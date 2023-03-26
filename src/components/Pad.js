5b43a7499cbbe26280645b00f01afcfc47287b28
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-03-26 15:47:15 +0530

    Update Pad.js

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,43 +1,26 @@
 import React from "react";
-import { bank1 } from "./App"
+import { bank1 } from "./App";
 
-function Pad({ handleClick, power, backgroundStyle, element, id }) {
-  const audioRef = React.useRef(null);
-  
-  React.useEffect(() => {
-    document.addEventListener("keydown", handleKeyPress);
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress);
-    }
-  });
-  
-  const handleKeyPress = e => {
-    if (e.keyCode === id.charCodeAt(0)) {
-      playSound();
-    }
-  }
-  
-  const playSound = () => {
-    if (power) {
-      audioRef.current.currentTime = 0;
-      audioRef.current.play();
-      handleClick(bank1[element].name);
-    }
-  }
-  
+function Pad({ handleClick, power, element, id }) {
+  const backgroundStyle = power ? "orange" : "gray";
   return (
     <button
       data-tag={id}
-      type='button'
-      className='drum-pad'
-      onClick={playSound}
+      type="button"
+      className="drum-pad"
+      onClick={handleClick}
       id={bank1[element].name}
       disabled={!power}
-      style={{background: `${backgroundStyle}`}}>
+      style={{ background: `${backgroundStyle}` }}
+    >
       {element}
-      <audio ref={audioRef} src={bank1[element].source} className='clip'></audio>
+      <audio
+        id={element}
+        src={bank1[element].source}
+        className="clip"
+      ></audio>
     </button>
   );
 }
 
-export default Pad;
+export default Pad;
